c9b226182925ee78337b9f6a027034ca44417851
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2016-12-07 14:24:05 -0200

    Disable 'apps' function (refs #15)

--- conf-base.php
+++ conf-base.php
@@ -3,6 +3,7 @@
 return [
     'app.enabled.projects'  => false,
     'app.enabled.events'    => false,
+    'app.enabled.apps'      => false,
     'maps.center'           => [-23.633201663141417, -46.6043472290039],
     'maps.zoom.default'     => 11,
 ];
