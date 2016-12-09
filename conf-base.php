be7e60651e67fcfc6623ca878188dc8c30832e53
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2016-12-09 16:10:31 -0200

    Change name in XLS Export (refs #16)

--- conf-base.php
+++ conf-base.php
@@ -6,4 +6,5 @@ return [
     'app.enabled.apps'      => false,
     'maps.center'           => [-23.633201663141417, -46.6043472290039],
     'maps.zoom.default'     => 11,
+    'export.excelName'      => 'gestao-de-hortas-dados-exportados.xls'
 ];
